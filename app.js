e1fd729afbf044cea8edd32b9998297fe228e4f5
Patrick Rauls <patrickrauls@Patricks-Air.domain>
2017-03-25 01:46:27 -0600

    Simplify to bare bones

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
 //modules
-const express = require('express'),
-    body_parser = require('body-parser');
+const express = require('express');
 
 //methods
-const app = express(),
-    json_parser = body_parser.json();
+const app = express();
 
 //routes
-const routes = require('./routes/index'),
-    me = require('./routes/me'),
-    users = require('./routes/users'),
-    orders = require('./routes/orders'),
-    invoices = require('./routes/invoices'),
-    products = require('./routes/products'),
-    inventory = require('./routes/inventory');
 
-
-//middleware
-app.use(json_parser);
-app.use(body_parser.urlencoded({
-    extended: true
-}))
 
 //routing
-app.use('/', routes);
-app.use('/me', me);
-app.use('/users', users);
-app.use('/orders', orders);
-app.use('/invoices', invoices);
-app.use('/products', products);
-app.use('/inventory', inventory);
 
+//export
 module.exports = app;
