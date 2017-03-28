eefdbd55032e52a6571409d75278731ca459c062
Patrick Rauls <patrickrauls@Patricks-Air.domain>
2017-03-28 15:07:16 -0600

    test email

--- routes/email.js
+++ routes/email.js
@@ -1,19 +1,32 @@
-const router = require('express').Router(),
-    sendgrid = require('sendgrid')(process.env.EMAIL_USER, process.env.EMAIL_PASSWORD);
+//modules
+const express = require('express'),
+    sendgrid = require('sendgrid');
 
-    
+//methods    
+const router = express.Router(),
+    helper = sendgrid.mail,
+    send = sendgrid(process.env.SENDGRID_KEY);
 
+const welcome = 'Thanks for getting in touch! We\'re making storage super easy and we\'re so excited to have you along for the ride! One of our team members will get in touch with you soon!';
 router.post('/', (req, res) => {
-    let email = req.body.email;
-    sendgrid.send({
-        to: email,
-        from: 'noreply@getruumy.com',
-        subject: 'Storage is about to become SUPER EASY',
-        text: 'Thanks for signing up to get updates on ruumy. We\'re super excited to have you along for the ride! One of our team members will be in touch with you soon with updates!'
-    }, (err, json) => {
-        return err ? console.error(err) : console.log(json)
+    let from = new helper.Email('noreply@getruumy.com'),
+        to = new helper.Email(req.body.email),
+        subject = 'Storage is about to get way easier!',
+        content = new helper.Content('text/plain', welcome),
+        mail = new helper.Mail(from, subject, to, content),
+        request = send.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: mail.toJSON()
+        });
+    send.API(request, (error, response) => {
+        if (err) {
+            console.error(err);
+            res.status(400).json({ message: 'Uh oh. There was a problem with your request. Please try again later' });
+        } else {
+            res.status(200).json({ message: 'Thanks! We\'ll Be In Touch!' });
+        }
     })
-    res.status(200).json({ message: 'Thanks! We\'ll Be In Touch!' });
 })
 
 router.post('/message', (req, res) => {
