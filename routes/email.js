b6ff8d0cac9fbf0b7f10ad3bd1b87306d9444e9a
Patrick Rauls <patrickrauls@Patricks-Air.domain>
2017-03-28 10:55:28 -0600

    initial commit

--- routes/email.js
+++ routes/email.js
@@ -1,10 +1,18 @@
 const router = require('express').Router(),
-    sendgrid = require('sendgrid');
+    sendgrid = require('sendgrid')(process.env.EMAIL_USER, process.env.EMAIL_;
 
     
 
 router.post('/', (req, res) => {
     let email = req.body.email;
+    sendgrid.send({
+        to: email,
+        from: 'noreply@getruumy.com',
+        subject: 'Storage is about to become SUPER EASY',
+        text: 'Thanks for signing up to get updates on ruumy. We\'re super excited to have you along for the ride! One of our team members will be in touch with you soon with updates!'
+    }, (err, json) => {
+        return err ? console.error(err) : console.log(json)
+    })
     res.status(200).json({ message: 'Thanks! We\'ll Be In Touch!' });
 })
 
