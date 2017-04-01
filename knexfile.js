f2d91a9b24f493f7113ba58f1d1ee77e8ecea177
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-04-01 16:44:31 -0600

    changing timeout

--- knexfile.js
+++ knexfile.js
@@ -21,8 +21,10 @@ module.exports = {
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
+      requestTimeout: 1
     },
+    acquireConnectionTimeout: 2,
     migrations: {
       tableName: 'knex_migrations'
     }
