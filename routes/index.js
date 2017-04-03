035affdf47e5d6b68efb156c98b31d6671a05681
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-04-03 12:51:07 -0600

    sending back session variables

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const express = require('express'),
 
 router.get('/', (req, res) => {
     req.session.redis = true;
-    res.status(200).json(docs)
+    res.status(200).json(req.session)
 })
 
 module.exports = router;
