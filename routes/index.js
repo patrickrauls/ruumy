d0b81708193b6071ee8d37e07328859fcaee47c8
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-04-04 09:52:03 -0600

    logging errors

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express'),
     query = require('../query'),
     argon2 = require('argon2'),
     salt = Buffer.from(process.env.SALT),
+    log = console.log,
     router = express.Router();
 
 
@@ -11,24 +12,33 @@ router.get('/', (req, res) => {
 router.post('/login', (req, res) => {
     query.read_user({ email: req.body.email })
         .then(user => {
+            log('user after read', user);
             return Promise.all([user, argon2.verify(user[0].password, req.body.password)])
         })
         .then(match => {
+            log(match);
             if (match[1]) {
                 req.session.user = {
                     id: match[0][0].id,
                     email: match[0][0].email
                 }
+                log('req.session after created', req.session)
                 res.status(200).json(req.session)
-            } else {
+            } elseif(req.session) {
                 req.session.destroy(error => {
+                    log('error in destroying session')
                     error ?
-                        res.status(500).send('error') :
+                        res.status(500).json(error) :
                         res.status(401).send('invalid email/password pair')
                 })
             }
+            else {
+                log('no session destroy needed')
+                res.status(401).send('invalid email/password pair')
+            }
         })
         .catch(error => {
+            log('error in reading user', error)
             res.status(500).json(error);
         })
 })
